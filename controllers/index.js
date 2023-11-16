ab367c4b771d802dc0521a586bec25b4ca312cf3
wnf <william@fluxrme.com>
2023-11-16 17:28:36 -0600

    adds branch for experimenting with weatherAPI

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,32 @@
 //INDEX CONTROLLER
-const token = process.env.CONGRESS_GOV_TOKEN;
-const ROOT_URL = 'https://api.congress.gov/';
+//const token = process.env.CONGRESS_GOV_TOKEN;
+// const ROOT_URL = 'https://api.congress.gov/';
+
+const token = process.env.WEATHERAPI_KEY
+const ROOT_URL = 'https://api.weatherapi.com/v1/';
+const endpoint = 'forecast'
 
 module.exports = {
-  index
+  // apiIndex,
+  index,
 }
 
 async function index(req, res, next) {
-  //const username = req.query.username;
-  const committeeMeetingIndexUrl = `${ROOT_URL}v3/committee-meeting?api_key=${token}&format=json`
-  const committeeMeetingsData = await fetch(committeeMeetingIndexUrl)
-  .then(res => res.json()) 
-  console.log (committeeMeetingsData)
 
-  const summariesIndexUrl = `${ROOT_URL}v3/summaries?api_key=${token}&format=json`
-  const summariesData = await fetch(summariesIndexUrl)
+  const endpointUrl = `${ROOT_URL}${endpoint}.json?key=${token}&q=70116&days=1&aqi=no&alerts=no`
+  const endpointData = await fetch(endpointUrl)
   .then(res => res.json()) 
-  console.log (summariesData)
-  
-  // const billText = await fetch(`https://www.congress.gov/118/bills/hr4394/BILLS-118hr4394rh.htm`)
-  // //.then(res => res.json())
-
-  // console.log(billText)
+  console.log (endpointUrl, endpointData.forecast.forecastday)
 
   res.render('index');
 }
+
+
+// async function index(req, res, next) {
+//   const summariesIndexUrl = `${ROOT_URL}v3/summaries?api_key=${token}&format=json`
+//   const summariesData = await fetch(summariesIndexUrl)
+//   .then(res => res.json()) 
+//   console.log (summariesData)
+
+//   res.render('index');
+// }
